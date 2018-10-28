2cdb6a8d6bcd5aaa4db2ca1b404a47b00d927c7b
Hamilton Santana <hamilton.santana@outlook.com>
2018-10-27 23:33:02 -0300

    Fix/Ajuste para rotas dinamicas

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ var app = express();
 
 app.listen(8080);
 
+const filename = '';
+
 app.get('/', (req, res) =>{
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Pagina Inicial</h1>');
 });
-app.get('/tag', (req, res) =>{
-  fs.readFile('src/tag.html', function(err, data){
+
+app.get('/:id', (req, res) =>{
+  console.log(req.params.id);
+  fs.readFile('src/'+req.params.id+'.html', function(err, data){
     if(err){
-      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.writeHead(404, {'Content-Tpe': 'text/html'});
+      console.log(err);
       return res.end("404 Not Found");
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
-  });
+  })
 });
