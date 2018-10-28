3a2abcc77f80c801aa89dcf5c0fb3f4ee093a32c
Hamilton Santana <hamilton.santana@outlook.com>
2018-10-27 23:20:38 -0300

    Fix rota. Chamada arquivo html

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ app.get('/', (req, res) =>{
   res.write('<h1>Pagina Inicial</h1>');
 });
 app.get('/tag', (req, res) =>{
-  fs.readFile('/src/tag.html', (err, data)=>{
+  fs.readFile('src/tag.html', function(err, data){
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
+    return res.end();
   });
 });
