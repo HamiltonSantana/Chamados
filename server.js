35a5bfbba45c6aad6ac7ac5aae50f88a7f3b257d
Hamilton Santana <hamilton.santana@outlook.com>
2018-10-27 23:12:03 -0300

    Adicionado manipulador de arquivo ao server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.listen(8080);
 
 app.get('/', (req, res) =>{
-  res.render(src/tag.html);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>Pagina Inicial</h1>');
+});
+app.get('/tag', (req, res) =>{
+  fis.readFile('/src/tag.html', data=>{
+
+  });
 });
