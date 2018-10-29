1eee05c3c3f2f317f283e0c0b19d4f3b065969fb
HamiltonSantos <hamilton.santana@outlook.com>
2018-10-29 11:31:39 -0300

    fix tratamento de URL no server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-app.listen(8080);
+app.listen(8080, ()=>{console.log('Servidor ativo!')});
 
 const filename = '';
 
@@ -11,16 +11,12 @@ app.get('/', (req, res) =>{
   res.write('<h1>Pagina Inicial</h1>');
 });
 
-// Tratar para chamada de JS. Ao 'loadar' o arquivo, o server
-// nao trata .js (MIME)
-app.get('/:id', (req, res) =>{
-  console.log(req.params.id);
-  fs.readFile('src/'+req.params.id+'.html', function(err, data){
-    if(err){
-      res.writeHead(404, {'Content-Tpe': 'text/html'});
-      console.log(err);
-      return res.end("404 Not Found");
-    }
+// Adicionando um tratamento para url
+app.get('/:fd/:id', (req, res)=>{
+  var filename = '.'+req.url;
+  console.log(filename);
+  fs.readFile(filename, (err, data)=>{
+    if(err) throw err;
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
